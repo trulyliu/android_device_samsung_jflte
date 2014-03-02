424103f71ccda61dc56a73cc10cff12f8020cdee
broodplank <guichardflorent@gmail.com>
2014-03-02 12:31:06 +0100

    Add back init.local.rc to ramdisk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -62,3 +62,11 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.led.sh
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.local.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.local.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
